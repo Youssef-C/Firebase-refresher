87634c1e5cc479a02d85ee548d0552c915e79496
Youssef-C <youssef.chardi@student.kyh.se>
2021-07-11 20:49:36 +0200

    page 27, adding EVN variables

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBqKfNoGoo7jvw0BI7iA2kY7XTYnCyTqn8",
+    authDomain: "react-refresher-9f950.firebaseapp.com",
+    projectId: "react-refresher-9f950",
+    storageBucket: "react-refresher-9f950.appspot.com",
+    messagingSenderId: "175689195718",
+    appId: "1:175689195718:web:d8cc34360e2b6d6882b7c7",
+    measurementId: "G-Y03HX5YJW8"
+  };
